468ad6c780afa4ae400abec331be9c6c765a1b63
Saklain Ahmad <saklainhmd@gmail.com>
2022-12-25 13:48:11 +0530

    add a controller and router for home

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-// //use express router
-// app.use('/', require('./routes'));
+//use express router
+app.use('/', require('./routes'));
 
 // // setup the view engine
 // app.set('view engine', 'ejs');
